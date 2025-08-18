579c8b5b094b65f479fa4e72d1355e7d1223069e
Lyes Rahal <lyes.rahal@univ-bouira.dz>
2025-08-18 17:23:03 +0100

    update settings

--- auth_main_2/settings.py
+++ auth_main_2/settings.py
@@ -173,8 +173,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True  
-
+# CORS_ALLOW_ALL_ORIGINS = True  
+CORS_ALLOWED_ORIGINS = [
+    "https://share-blood-v2-bhhm.vercel.app",
+]
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
